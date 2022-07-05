128f674cd1b9eb80f99ce16048f985ae70448695
kleva-j <kasmickleva@gmail.com>
2022-07-05 15:39:31 +0100

    add document page with link to fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { createGetInitialProps } from "@mantine/next";
+
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300&display=swap"
+          rel="stylesheet"
+        />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
