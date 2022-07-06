84b0eaebdbad4489d192bb64e8f4cccf15d0910e
kleva-j <kasmickleva@gmail.com>
2022-07-06 03:08:42 +0100

    modify page endpoint to fetch 10 jobs

--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,30 +1,38 @@
-import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { GetServerSideProps, NextPage } from "next";
 import { JobsComponent } from "src/components/Jobs";
-import { fetchArbeitJobs } from "src/util/scraper";
-import { fetchJobs } from "src/util";
+import { fetchJobs, GroupByKey } from "src/util";
+import { AxiosRequestConfig } from "axios";
 
-type jobsType = AxiosResponse<any, any> | any[];
+type jobsType = { [key: string]: any };
 
 let jobSources: {
   [key: string]: (params: AxiosRequestConfig) => Promise<jobsType>;
 } = {
-  arbeit: fetchArbeitJobs,
   swiss: fetchJobs(process.env.NEXT_PUBLIC_SWISS_JOBS || ""),
   devitjobs: fetchJobs(process.env.NEXT_PUBLIC_DEVITJOBS || ""),
 };
 
-const Jobs: NextPage<{ jobs: any }> = (props) => {
-  return <JobsComponent jobs={props.jobs} />;
-};
+interface JobsPage {
+  jobs: any[];
+  locations: string[];
+  country: string[];
+}
+
+const Jobs: NextPage<JobsPage> = ({ jobs }) => <JobsComponent jobs={jobs} />;
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=10, stale-while-revalidate=59"
   );
-
-  return { props: { jobs: await jobSources["arbeit"]({}) } };
+  let { jobs } = await jobSources["devitjobs"]({});
+  let jobSlice = jobs["job"].slice(0, 10);
+  return {
+    props: {
+      jobs: jobSlice,
+      ...GroupByKey(jobSlice, "location", "country"),
+    },
+  };
 };
 
 export default Jobs;
