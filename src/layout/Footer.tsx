736a04446a50492e92ff1d2a039ddf63d6dd0bfc
kleva-j <kasmickleva@gmail.com>
2022-07-06 03:07:56 +0100

    update footer to match design

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,11 @@
-import { Footer } from "@mantine/core";
+import { Footer, Container, Text, Title } from "@mantine/core";
 
 export const FooterMenu = () => {
   return (
     <Footer height={60} p="md">
-      Application footer
+      <Container size="xl" style={{ display: "flex" }}>
+        <Title style={{ fontSize: "11px" }} order={6}>Built by <Text size="sm" color="yellow" underline inherit component="span">Kasmickleva</Text></Title>
+      </Container>
     </Footer>
   );
 };
