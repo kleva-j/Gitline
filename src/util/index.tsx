3c6fdcb78ede35de80519d0fbcfcee52d99691d4
kleva-j <kasmickleva@gmail.com>
2022-07-08 21:38:30 +0100

    add darker colormixer theme and  utility function to format salary

--- src/util/index.tsx
+++ src/util/index.tsx
@@ -17,6 +17,7 @@ export const colorMixer = (
   return {
     light: theme.fn.darken(theme.colors[mode][1], 0.01),
     dark: theme.colors[mode][4],
+    darker: theme.fn.darken(theme.colors[mode][4], 0.09),
   };
 };
 
@@ -27,9 +28,17 @@ export const GroupByKey = (list: any[], ...args: string[]) => {
       if (!keys[item]) keys[item] = [];
       if (!keys[item].includes(curr[item])) {
         let arr = keys[item];
-        keys[item] = arr.concat([curr[item]])
+        keys[item] = arr.concat([curr[item]]);
       }
-    })
+    });
     return keys;
   }, {});
+};
+
+export const formatSalary = (salary: string) => {
+  let [min, max] = salary
+    .split("GBP per year")[0]
+    .split(" - ")
+    .map((val) => parseInt(val))
+  return `£${min}k - £${max}k`
 };
