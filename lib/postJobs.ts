fab95e1713d99d721a86e31274ed9651e36d8ae2
kleva-j <kasmickleva@gmail.com>
2022-07-15 02:20:46 +0100

    fix linting error

--- lib/postJobs.ts
+++ lib/postJobs.ts
@@ -1,26 +1,14 @@
-import { fetchArbeitJobs } from "src/util/scraper";
-import { AxiosRequestConfig } from "axios";
 import { fetchJobs } from "src/util";
 
 import clientPromise from "./mongodb";
 
-type jobsType = { [key: string]: any };
-
-let jobSources: {
-  [key: string]: (params: AxiosRequestConfig) => Promise<jobsType>;
-} = {
-  arbeit: fetchArbeitJobs,
-  swiss: fetchJobs(process.env.NEXT_PUBLIC_SWISS_JOBS || ""),
-  devitjobs: fetchJobs(process.env.NEXT_PUBLIC_DEVITJOBS || ""),
-};
-
 export default async function postJobs(): Promise<any> {
   const client = await clientPromise;
   try {
-    let { jobs } = await jobSources["devitjobs"]({});
+    let { jobs } = await fetchJobs(process.env.NEXT_PUBLIC_DEVITJOBS ?? "")({});
     const collection = await client.db("gitline-sample").collection("jobs");
     await collection.deleteMany();
-    await collection.insertMany(jobs["job"], {});
+    await collection.insertMany(jobs.job, {});
   } catch (err) {
     throw err;
   }
